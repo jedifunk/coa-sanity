93302d4a4678bf9dd5aeba872bb1a312fe09baee
jedifunk <jedifunk@jedifunk-pro.local>
2022-01-07 11:46:54 -0600

    added dashboard, gatsby cloud widget

---
+++ parts/dashboardConfig.js
@@ -0,0 +1,20 @@
+export default {
+  widgets: [
+    {
+      name: 'project-users',
+    },
+    {
+      name: 'project-info',
+    },
+    {
+      name: 'gatsby',
+      options: {
+        sites: [
+          {
+            siteUrl: ''
+          }
+        ]
+      }
+    }
+  ]
+}
