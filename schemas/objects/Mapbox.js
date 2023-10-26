ce0038ba6d509f1cf6a879aa9727f6424e12d9ff
Bryce Flory <iam@bryceflory.com>
2023-10-26 16:23:00 +0100

    updated mapbox object

--- schemas/objects/Mapbox.js
+++ schemas/objects/Mapbox.js
@@ -7,6 +7,18 @@ export default {
       title: "Title",
       name: "title",
       type: "string"
+    },
+    {
+      title: 'Map',
+      name: 'map',
+      type: 'reference',
+      to: [{type: 'map'}]
     }
-  ]
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      //subtitle: 'map'
+    }
+  }
 }
