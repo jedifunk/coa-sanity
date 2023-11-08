36bbd1b5c06a3644fd3357b5dd42c3a8f09e2e8b
Bryce Flory <iam@bryceflory.com>
2023-11-08 16:40:54 +0000

    moved to JS library from web services for Places API to fix CORS

--- components/input/Geometry.jsx
+++ components/input/Geometry.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from 'react'
+import React, {useState, useCallback, useRef, useEffect} from 'react'
 import { useFormValue, set, unset } from 'sanity'
 import { Autocomplete, Box, TextInput, Flex, Label, Stack, Card, Text } from '@sanity/ui'
 import {AiOutlineSearch} from 'react-icons/ai'
@@ -10,55 +10,66 @@ const Geometry = (props) => {
   const { value, onChange } = props
   const [predictions, setPredictions] = useState([])
 
+  const autocompleteService = useRef()
+  const placesService = useRef()
+
+  useEffect(() => {
+    loadGoogleMapsApi()
+  }, [])
+
+  const loadGoogleMapsApi = () => {
+    const script = document.createElement('script')
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${GM_KEY}&libraries=places`
+    script.async = true
+    script.defer = true
+    script.onload = () => {
+      autocompleteService.current = new window.google.maps.places.AutocompleteService()
+      placesService.current = new window.google.maps.places.PlacesService(document.createElement('div'))
+    }
+    document.body.appendChild(script)
+  }
+
   const handleSelect = useCallback((e) => {
     fetchCoordinates(e)
   })
 
   const handleChange = useCallback((newValue) => {
+
     onChange(newValue ? set(
       {
         'geoName': newValue.name, 
-        'latitude': newValue.geometry.location.lat, 
-        'longitude': newValue.geometry.location.lng,
+        'latitude': newValue.geometry.location.lat(), 
+        'longitude': newValue.geometry.location.lng(),
         'mapBounds': {
-          'northeast': [newValue.geometry.viewport.northeast.lng, newValue.geometry.viewport.northeast.lat], 
-          'southwest': [newValue.geometry.viewport.southwest.lng, newValue.geometry.viewport.southwest.lat]
+          'northeast': [newValue.geometry.viewport.Oa.hi, newValue.geometry.viewport.mb.hi], 
+          'southwest': [newValue.geometry.viewport.Oa.lo, newValue.geometry.viewport.mb.lo]
         }
       }
     ) : unset())
   }, [onChange])
 
   const handleSearch = async (query) => {
-    const fetchQuery = docType === 'city' ? `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${query}&radius=50000&types=(cities)&key=${GM_KEY}` : `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${query}&radius=50000&key=${GM_KEY}`
-    
-    try {
-      const response = await fetch(fetchQuery)
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`)
-      }
-      const data = await response.json()
-      const formattedResults = data.predictions.map(prediction => ({ 
-        value: prediction.place_id, 
-        name: prediction.structured_formatting.main_text ,
-        description: prediction.description
-      }))
-      setPredictions(formattedResults)
-    } catch (error) {
-      console.error('Error fetching suggestions:', error);
+    if (autocompleteService.current) {
+      autocompleteService.current.getPlacePredictions({ input: query }, (predictions, status) => {
+        if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+          const formattedResults = predictions.map(prediction => ({ 
+            value: prediction.place_id, 
+            name: prediction.structured_formatting.main_text ,
+            description: prediction.description
+          }))
+          setPredictions(formattedResults)
+        }
+      })
     }
   }
 
-  const fetchCoordinates = async (location) => {
-    if (location !== undefined) {
-      await fetch(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${location}&fields=name,geometry,type,place_id&key=${GM_KEY}`)
-      .then(response => response.json())
-      .then(data => {
-        const coors = data.result
-        handleChange(coors)
+  const fetchCoordinates = async (placeId) => {
+    if (placesService.current) {
+      placesService.current.getDetails({ placeId: placeId, fields: ['name', 'geometry'] }, (place, status) => {
+        if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+          handleChange(place)
+        }
       })
-      .catch(error => console.error('fetchCoordinates:', error))
-    } else {
-      console.log('Nope')
     }
   }
 
